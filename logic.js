b583723d96131b8f6c45dec4dc4ca5d907a8b121
GuitarGuy244 <rgsl.salazar@gmail.com>
2019-06-01 21:23:00 -0700

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,61 +1,63 @@
 var topics = ["guitar", "piano", "drums", "bass", "flute", "naruto", "venom", "computers", "skyrim", "fallout"];
 
-function diplayGif()
-{
+
+ function displayGif() {
+
     var topic = $(this).attr("data-name");
+  
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=58Kw1KJaCnhqmZ84jnKoevMK0LVaASRp&q="+ topic +"&limit=10&offset=0&rating=R&lang=en";
 
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response)
-    {
-        for (var i = 0; i < 10; i++)
-        {
-           
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+  
+       console.log(response)
+      for (var i = 0; i < 10; i++)
+      {
+       var gifDiv = $("#gif-view");
+  
+       var imgUrl = response.data[i].images.original.url;
 
-            var gifID = $("#gif-view");
-            var imgUrl = response.data[i].images.preview_webp.url;
-            var image = $("<img>").attr("src", imgUrl);
-
-            gifID.append(image);
-        }
+       var image = $("<img>").attr("src", imgUrl);
+  
+       gifDiv.append(image);
+      }
     });
-}
-
-function renderButtons()
-{
-
-    $("#buttons-view").empty();
-
-    for (var i = 0; i < topics.length; i++)
-    {
-
-        var a = $("<button>");
-        
-        a.addClass("gif-button");
-        
-        a.attr("data-name", topics[i]);
-        
-        a.text(topics[i]);
-        
-        $("#buttons-view").append(a);
-    }
-}
-
-$("#add-gif").on("click", function(event)
-{
- 
-    event.preventDefault();
+  }
+  
+  function renderButtons() {
+  
+   $("#buttons-view").empty();
+  
+   for (var i = 0; i < topics.length; i++) 
+   {
+  
+     var a = $("<button>");
+     
+     a.addClass("gif-btn");
+     
+     a.attr("data-name", topics[i]);
     
-    var gifs = $("#gif-input").val().trim();
+     a.text(topics[i]);
+     
+     $("#buttons-view").append(a);
+   }
+  }
+  
+  
+  $("#add-gif").on("click", function(event)
+  {
    
-    topics.push(gifs);
+   event.preventDefault();
    
-    renderButtons();
-});
-
-$(document).on("click", ".gif-button", displayGif());
-
-diplayGif();
-renderButtons();
+   var gifs = $("#gif-input").val().trim();
+  
+   topics.push(gifs);
+  
+   renderButtons();
+  });
+  
+  $(document).on("click", ".gif-btn", displayGif);
+  
+  renderButtons()
